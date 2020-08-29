8716d347e1f6876e5f72f3b441baa148d5eee40d
Aaron Larsen <>
2020-08-29 13:02:51 -0600

    Replicate final results from burgers equation-Summer 2020

---
+++ Burgers/replicate.sh
@@ -0,0 +1,6 @@
+python burgers.py --train
+python burgers.py --res
+python burgers_model.py ModelTest
+python burgers.py --test ModelTest
+python plot_burgers.py --save
+python overlap_burgers.py --save
