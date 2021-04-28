a964048825b8b38c3537dbab18f27f016d3b85d9
Aaron Larsen <aaronml@byu.edu>
2021-04-28 13:46:34 -0600

    add model to commands

--- Burgers/replicate.sh
+++ Burgers/replicate.sh
@@ -1,6 +1,6 @@
 python burgers.py --train
 python burgers.py --res
 python model_burgers.py ModelTest
-python burgers.py --test ModelTest
-python plot_burgers.py --save
+python burgers.py --test --model ModelTest
+python plot_burgers.py -w --save
 python overlap_burgers.py --save
